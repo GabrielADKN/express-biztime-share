2b583bdf773602a26f59ba65f8b1e8063d6b37ff
gabrieladkn <adoukonoubienheureux@gmail.com>
2024-02-23 13:57:43 -0500

    Add a Database

--- db.js
+++ db.js
@@ -1,2 +1,19 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+    DB_URI = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/biztime_test`;
+} else {
+    DB_URI = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/biztime`;
+}
+
+let db = new Client({
+    connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
